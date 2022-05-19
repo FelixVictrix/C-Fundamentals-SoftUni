5b0190210401430c4104a0fbf052bab8f5196ce8
FelixVictrix <tinchevp@gmail.com>
2022-05-19 19:58:25 +0300

    0

---
+++ 04. List of Products/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<string> listOfProducts = new List<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                listOfProducts.Add(Console.ReadLine());
+            }
+            listOfProducts.Sort();
+            for (int i = 0; i < listOfProducts.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{listOfProducts[i]}");
+            }
+        }
+    }
+}
