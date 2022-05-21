d316d274fb26dfc06d835ff61231a0eed2e25351
FelixVictrix <tinchevp@gmail.com>
2022-05-21 18:54:00 +0300

    0

--- 05. Bomb Numbers/Program.cs
+++ 05. Bomb Numbers/Program.cs
@@ -21,10 +21,31 @@ namespace _05._Bomb_Numbers
             int bombIndex = bomb[0];
             int power = bomb[1];
 
-            if (bomb.Sum() < list.Count)
+            for (int i = 0; i < list.Count - 1; i++)
             {
-             
+                int currNum = list[i];
+                if (currNum == bombIndex)
+                {
+                    int startIndex = i - power;
+                    int endIndex = i + power;
+
+                    if (startIndex < 0)
+                    {
+                        startIndex = 0;
+                    }
+
+                    if (endIndex > list.Count - 1)
+                    {
+                        endIndex = list.Count - 1;
+                    }
+
+                    int explosionArea = endIndex - startIndex + 1;
+                    list.RemoveRange(startIndex, explosionArea);
+                    i = startIndex - 1;
+                }
             }
+
+            Console.WriteLine(list.Sum());
         }
     }
 }
