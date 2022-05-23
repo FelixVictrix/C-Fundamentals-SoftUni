64765b960385aba1d7f19e231d446b871c16499e
FelixVictrix <tinchevp@gmail.com>
2022-05-23 19:36:29 +0300

    0

---
+++ 01. Messaging/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> intList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string text = Console.ReadLine();
+
+            List<char> chrList = text.ToList();
+
+            string message = string.Empty;
+
+            for (int i = 0; i < intList.Count; i++)
+            {
+                int index = Sum(intList , i);
+
+                if (index > chrList.Count - 1)
+                {
+                    index = index - chrList.Count;
+                }
+                message += chrList[index];
+                chrList.RemoveAt(index);
+            }
+
+            Console.WriteLine(message);
+
+        }
+
+        static int Sum(List<int> intList , int i)
+        {
+            string numbers = intList[i].ToString();
+            int sum = 0;
+
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                string num = numbers[j].ToString();
+                sum += int.Parse(num);
+            }
+            return sum;
+        }
+    }
+}
---
+++ 02. Car Race/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Car_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> raceList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            double leftTime = 0;
+            double rightTime = 0;
+            int endOfTheRace = raceList.Count / 2;
+
+            for (int i = 0; i < endOfTheRace; i++)
+            {
+                leftTime += raceList[i];
+                rightTime += raceList[raceList.Count - 1 - i];
+
+                if (raceList[i] == 0)
+                {
+                    leftTime *= 0.8;
+                }
+
+                if (raceList[raceList.Count - 1 - i] == 0)
+                {
+                    rightTime *= 0.8;
+                }
+
+            }
+
+            if (leftTime < rightTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {leftTime:f}");
+            }
+            else if (rightTime < leftTime)
+            {
+                Console.WriteLine($"The winner is right with total time: {rightTime:f}");
+            }
+        }
+    }
+}
