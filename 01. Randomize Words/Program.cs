183466f732b9b05e053efa15b469e6a5a656fadc
FelixVictrix <tinchevp@gmail.com>
2022-05-25 19:28:18 +0300

    0

---
+++ 01. Randomize Words/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string[] words = Console.ReadLine().Split();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < words.Length-1; i++)
+            {
+                
+                string a = rnd.Next(minValue,maxValue);
+
+            }
+
+        }
+    }
+}
---
+++ 03. Songs/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numOfSongs = int.Parse(Console.ReadLine());
+
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < numOfSongs; i++)
+            {
+                string[] currSong = Console.ReadLine().Split('_');
+                string type = currSong[0];
+                string name = currSong[1];
+                string time = currSong[2];
+
+                var song = new Song(type, name, time);
+                songs.Add(song);
+            }
+            string printType = Console.ReadLine();
+
+            if (printType == "all")
+            {
+                foreach (var song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (var sing in songs.Where(x => x.TypeList == printType))
+                {
+                    Console.WriteLine(sing.Name);
+                }
+            }
+
+        }
+
+    }
+    public class Song
+    {
+        public Song(string typeList, string name, string time)
+        {
+            TypeList = typeList;
+            Name = name;
+            Time = time;
+        }
+
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+
+    }
+}
