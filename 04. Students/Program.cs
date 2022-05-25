366d1d062d8d809ffe0acbdb7b9b7e9bfb77b048
FelixVictrix <tinchevp@gmail.com>
2022-05-25 21:23:41 +0300

    0

---
+++ 04. Students/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+          List<Student> students = new List<Student>();
+
+            string input = Console.ReadLine();
+
+            while (input!="end")
+            {
+                string[] studentInfo = input.Split();
+                string firstName = studentInfo[0];
+                string lastName = studentInfo[1];
+                int age= int.Parse(studentInfo[2]);
+                string hometown = studentInfo[3];
+
+                Student student = new Student();
+                student.FirstName = firstName;
+                student.LastName = lastName;
+                student.Age= age;
+                student.HomeTown = hometown;
+
+                students.Add(student);
+
+                input = Console.ReadLine();
+            }
+
+            string townName = Console.ReadLine();
+
+            foreach (var student in students.Where(x=> x.HomeTown == townName))
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+    }
+
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+
+    }
+}
---
+++ 05. Students 2.0/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Students_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Student> students = new List<Student>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] studentInfo = input.Split();
+                string firstName = studentInfo[0];
+                string lastName = studentInfo[1];
+                int age = int.Parse(studentInfo[2]);
+                string hometown = studentInfo[3];
+
+                Student student = new Student();
+                student.FirstName = firstName;
+                student.LastName = lastName;
+                student.Age = age;
+                student.HomeTown = hometown;
+
+                var a = students.Where(x => x.FirstName == firstName);
+
+                if (a)
+                {
+                    
+
+                }
+
+                foreach (var name in students.Where(x=> x.FirstName == firstName))
+                {
+                   name.FirstName = firstName;
+                   name.LastName = lastName;
+                   name.Age=age;
+                   name.HomeTown = hometown;
+                  
+                }
+
+                students.Add(student);
+
+                input = Console.ReadLine();
+            }
+
+            string townName = Console.ReadLine();
+
+            foreach (var student in students.Where(x => x.HomeTown == townName))
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+    }
+
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+
+    }
+}
