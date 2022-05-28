41c5c5671c63af36021f9b3409930e4295d37d2d
FelixVictrix <tinchevp@gmail.com>
2022-05-28 17:04:30 +0300

    0

--- 07. Vehicle Catalogue/Program.cs
+++ 07. Vehicle Catalogue/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 
 namespace _07._Vehicle_Catalogue
 {
@@ -7,19 +9,61 @@ namespace _07._Vehicle_Catalogue
     {
         static void Main(string[] args)
         {
-           
-            Vehicle car = new Cars();
+            //{type}/{brand}/{model}/{horse power / weight}
+            string input = Console.ReadLine();
+            Catalog catalog = new Catalog();
+
+            while (input != "end")
+            {
+                string[] carInf = input
+                    .Split('/', StringSplitOptions.RemoveEmptyEntries);
+
+                string type = carInf[0];
+                string brand = carInf[1];
+                string model = carInf[2];
+
+                Vehicle vehicle = null;
+
+                if (type == "Car")
+                {
+                    int horsePower = int.Parse(carInf[3]);
+                    vehicle = new Cars(brand, model, horsePower);
+
+                }
+                else if (type == "Truck")
+                {
+                    int weight = int.Parse(carInf[3]);
+                    vehicle = new Trucks(brand, model, weight);
+                }
+                catalog.Collections.Add(vehicle);
+
+                input = Console.ReadLine();
+            }
+
+            var catalogs = catalog.Collections.OrderBy(x => x.Brand).ToList();
+
+            Console.WriteLine($"Cars: ");
+            foreach (Cars vehicle in catalogs.Where(c => c.GetType().Name == "Cars"))
+            {
+                Console.WriteLine($"{vehicle.Brand}: {vehicle.Model} - {vehicle.HorsePower}hp");
+            }
+            Console.WriteLine($"Trucks: ");
+            foreach (Trucks trck in catalogs.Where(c => c.GetType().Name == "Trucks"))
+            {
+                Console.WriteLine($"{trck.Brand}: {trck.Model} - {trck.Weight}kg");
+            }
 
         }
     }
     class Catalog
     {
-        private List<Vehicle> collections;
-
         public Catalog()
         {
-            collections = new List<Vehicle>();
+            Collections = new List<Vehicle>();
         }
+
+        public List<Vehicle> Collections { get; set; }
+
     }
     class Vehicle
     {
@@ -34,7 +78,7 @@ namespace _07._Vehicle_Catalogue
     }
     class Cars : Vehicle
     {
-        public Cars(string brand, string model, double horsePower) 
+        public Cars(string brand, string model, double horsePower)
             : base(brand, model)
         {
             HorsePower = horsePower;
