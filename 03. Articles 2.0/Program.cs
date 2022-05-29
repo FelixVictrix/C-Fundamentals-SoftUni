71616f0ef62824651a9cf9db6c148d47b625c74f
FelixVictrix <tinchevp@gmail.com>
2022-05-29 16:33:19 +0300

    0

---
+++ 03. Articles 2.0/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Articles_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Article> articles = new List<Article>();
+
+            Article article;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                var title = input[0];
+                var content = input[1];
+                var author = input[2];
+
+                article = new Article(title, content, author);
+                articles.Add(article);
+
+            }
+
+            string criteria = Console.ReadLine();
+
+            if (criteria == "title")
+            {
+               articles =  articles.OrderBy(x => x.Title).ToList();
+            }
+            else if (criteria == "content")
+            {
+                articles = articles.OrderBy(x => x.Content).ToList();
+            }
+            else if (criteria == "author")
+            {
+                articles = articles.OrderBy(x => x.Author).ToList();
+            }
+
+            foreach (Article article1 in articles)
+            {
+                Console.WriteLine(article1.ToString());
+            }
+        }
+    }
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; private set; }
+        public string Author { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+
+}
---
+++ 04t. Students/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04t._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int countOfStudents = int.Parse(Console.ReadLine());
+
+            List<Students> students = new List<Students>();
+
+            for (int i = 0; i < countOfStudents; i++)
+            {
+                string[] studentInfo = Console.ReadLine().Split();
+                string firstName = studentInfo[0];
+                string lastName = studentInfo[1];
+                double grade = double.Parse(studentInfo[2]);
+
+                students.Add(new Students(firstName, lastName, grade));
+            }
+
+            students = students.OrderByDescending(s => s.Grade).ToList();
+
+            foreach (Students student in students)
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+    }
+
+    class Students
+    {
+        public Students(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:f2}";
+        }
+    }
+}
