11ef02c1a974f78ccf902ef607e1f46568bddcaa
FelixVictrix <tinchevp@gmail.com>
2022-06-02 20:53:23 +0300

    0

---
+++ 02. A Miner Task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            string input = Console.ReadLine();
+            while (input != "stop")
+            {
+                string product = input;
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!dictionary.ContainsKey(product))
+                {
+                    dictionary.Add(product, 0);
+                }
+                dictionary[product] += quantity;
+
+                input = Console.ReadLine();
+            }
+            foreach (var kvp in dictionary)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ 03. Legendary Farming/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Legendary_Farming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> materials = new Dictionary<string, int>();
+            bool isTrue = true;
+
+            while (isTrue)
+            {
+                var input = Console.ReadLine().ToLower().Split();
+                AddDic(input, materials);
+                isTrue = materials.All(m => m.Value < 250);
+            }
+
+            var legendary = materials.FirstOrDefault(m => m.Value >= 250);
+            IsLegendary(legendary, materials);
+
+            materials.OrderByDescending(m => m.Value);
+            foreach (var kvp in materials)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+
+        }
+
+        static void IsLegendary(KeyValuePair<string, int> legendary, Dictionary<string, int> materials)
+        {
+            string isLegend = null;
+            switch (legendary.Key)
+            {
+                case "shards":
+                    isLegend = "Shadowmourne";
+                    break;
+                case "fragments":
+                    isLegend = "Valanyr";
+                    break;
+                case "motes":
+                    isLegend = "Dragonwrath";
+                    break;
+            }
+            Console.WriteLine($"{isLegend} obtained!");
+            materials[legendary.Key] -= 250;
+        }
+
+        static void AddDic(string[] input, Dictionary<string, int> materials)
+        {
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                var type = input[i + 1];
+                var quantity = int.Parse(input[i]);
+
+                if (!materials.ContainsKey(type))
+                {
+                    materials.Add(type, 0);
+                }
+                materials[type] += quantity;
+            }
+        }
+    }
+}
