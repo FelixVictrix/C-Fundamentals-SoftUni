99e7d196af27b0089e0bd84c86430b4b823f992d
FelixVictrix <tinchevp@gmail.com>
2022-06-06 17:36:05 +0300

    0

--- 08. Company Users/Program.cs
+++ 08. Company Users/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _08._Company_Users
 {
@@ -6,7 +8,37 @@ namespace _08._Company_Users
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Dictionary<string,List<string>> companyList = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] employeeInfo = input
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+                string companyName = employeeInfo[0];
+                string employeeId = employeeInfo[1];
+
+                if (!companyList.ContainsKey(companyName))
+                {
+                    companyList.Add(companyName, new List<string>());
+                }
+                if (!companyList[companyName].Contains(employeeId))
+                {
+                    companyList[companyName].Add(employeeId);
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var company in companyList.OrderBy(x=> x.Key))
+            {
+                Console.WriteLine(company.Key);
+                foreach (var item in company.Value)
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+            }
         }
     }
 }
