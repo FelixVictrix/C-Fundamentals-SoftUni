315be5936404c20ed4ac2e8d76b342d4c75ba003
FelixVictrix <tinchevp@gmail.com>
2022-06-08 13:35:33 +0300

    0

---
+++ 03. Substring/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+            string text = Console.ReadLine();
+
+            int indexOfWord = text.IndexOf(word);
+
+            while (indexOfWord >= 0)
+            {
+
+                
+
+                text = text.Remove(indexOfWord,word.Length);
+                indexOfWord = text.IndexOf(word);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
