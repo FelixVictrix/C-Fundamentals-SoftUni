fbc9619f0aa50000b1bcabaf2b69554da3fae8d3
FelixVictrix <tinchevp@gmail.com>
2022-06-08 16:28:51 +0300

    0

---
+++ 04. Text Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] words = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (var item in words)
+            {
+                text = text.Replace(item, new string('*', item.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ 05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string text = Console.ReadLine();
+
+            StringBuilder isDigit = new StringBuilder();
+            StringBuilder isLetter = new StringBuilder();
+            StringBuilder isElse = new StringBuilder();
+
+            foreach (var item in text)
+            {
+
+                if (char.IsDigit(item))
+                {
+                    isDigit.Append(item);
+                   
+                }
+                else if (char.IsLetter(item))
+                {
+                    isLetter.Append(item);
+
+                }
+               else
+                {
+                    isElse.Append(item);
+
+                }
+            }
+            Console.WriteLine(isDigit);
+            Console.WriteLine(isLetter);
+            Console.WriteLine(isElse);
+        }
+    }
+}
