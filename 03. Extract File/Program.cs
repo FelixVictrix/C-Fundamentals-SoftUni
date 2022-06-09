d3d26c615277c4d1e2e195093ce16faaec4e2b46
FelixVictrix <tinchevp@gmail.com>
2022-06-09 17:25:50 +0300

    0

--- 03. Extract File/Program.cs
+++ 03. Extract File/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _03._Extract_File
 {
@@ -6,14 +7,21 @@ namespace _03._Extract_File
     {
         static void Main(string[] args)
         {
-
             string filePath = Console.ReadLine();
-            int index = filePath.LastIndexOf('\\');
-            string nameType = filePath.Substring(index + 1);
-            string[] file = nameType.Split(".");
 
-            Console.WriteLine($"File name: {file[0]}");
-            Console.WriteLine($"File extension: {file[1]}");
+            var fileName = Path.GetFileNameWithoutExtension(filePath);
+            var fileExtension = Path.GetExtension(filePath);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+
+            //string filePath = Console.ReadLine();
+            //int index = filePath.LastIndexOf('\\');
+            //string nameType = filePath.Substring(index + 1);
+            //string[] file = nameType.Split(".");
+
+            //Console.WriteLine($"File name: {file[0]}");
+            //Console.WriteLine($"File extension: {file[1]}");
 
         }
     }
---
+++ 04. Caesar Cipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var letter in text)
+            {
+                int encrypted = ((int)letter + 3);
+                sb.Append((char)encrypted);
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
