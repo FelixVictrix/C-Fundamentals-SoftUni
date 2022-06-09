10df140d89504afe8028c8d9259f6fd8203e88ac
FelixVictrix <tinchevp@gmail.com>
2022-06-09 12:41:24 +0300

    0

--- 02. Character Multiplier/Program.cs
+++ 02. Character Multiplier/Program.cs
@@ -6,9 +6,45 @@ namespace _02._Character_Multiplier
     {
         static void Main(string[] args)
         {
-            
 
+            string[] input = Console.ReadLine().Split();
 
+            Console.WriteLine(Multiply(input));
+        }
+
+        static int Multiply(string[] input)
+        {
+            string firstString = input[0];
+            string secondString = input[1];
+            int sum = 0;
+
+            if (firstString.Length <= secondString.Length)
+            {
+                for (int i = 0; i < firstString.Length; i++)
+                {
+                    sum += firstString[i] * secondString[i];
+                }
+                secondString = secondString.Substring(firstString.Length);
+                for (int j = 0; j < secondString.Length; j++)
+                {
+                    sum += secondString[j];
+                }
+
+            }
+            else
+            {
+                for (int i = 0; i < secondString.Length; i++)
+                {
+                    sum += firstString[i] * secondString[i];
+                }
+                firstString = firstString.Substring(secondString.Length);
+                for (int j = 0; j < firstString.Length; j++)
+                {
+                    sum += firstString[j];
+                }
+            }
+
+            return sum;
         }
     }
 }
---
+++ 03. Extract File/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string filePath = Console.ReadLine();
+            int index = filePath.LastIndexOf('\\');
+            string nameType = filePath.Substring(index + 1);
+            string[] file = nameType.Split(".");
+
+            Console.WriteLine($"File name: {file[0]}");
+            Console.WriteLine($"File extension: {file[1]}");
+
+        }
+    }
+}
