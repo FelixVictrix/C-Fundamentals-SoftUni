def20aa95a72dce8a231dd721336b12571599cbf
FelixVictrix <tinchevp@gmail.com>
2022-06-09 19:00:56 +0300

    0

---
+++ 05. Multiply Big Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Multiply_Big_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bigNUm = Console.ReadLine();
+            int multiplier = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+            int numLeft = 0;
+
+            if (multiplier == 0 || bigNUm == "0")
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            for (int i = bigNUm.Length - 1; i >= 0; i--)
+            {
+
+                int curr = int.Parse(bigNUm[i].ToString());
+                int multiply = curr * multiplier + numLeft;
+
+                int numToAdd = multiply % 10;
+                numLeft = multiply / 10;
+
+                sb.Insert(0, numToAdd);
+
+            }
+            if (numLeft > 0)
+            {
+                sb.Insert(0, numLeft);
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ 06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace _06._Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                char first = input[i];
+                char second = input[i + 1];
+
+                if (first != second)
+                {
+                    sb.Append(first);
+                }
+               
+            }
+            sb.Append(input[input.Length - 1]);
+
+            Console.WriteLine(sb);
+        }
+    }
+}
