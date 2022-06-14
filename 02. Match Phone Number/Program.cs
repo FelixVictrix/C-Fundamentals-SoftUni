c5151bcaacee277c11c126a8dff633ab274f5d2e
FelixVictrix <tinchevp@gmail.com>
2022-06-14 16:04:49 +0300

    0

--- 02. Match Phone Number/Program.cs
+++ 02. Match Phone Number/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _02._Match_Phone_Number
 {
@@ -6,7 +8,23 @@ namespace _02._Match_Phone_Number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+           // var regex = @"\+[359]+([ -])[2]\1[0-9]{3}\1[0-9]{4}\b";
+            var phones = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(phones, @"\+[359]+([ -])2\1[0-9]{3}\1[0-9]{4}\b");
+
+            var matchedPhones = matches
+                .Cast<Match>()
+                .Select(x => x.Value.Trim())
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+
+            //foreach (Match item in matches)
+            //{
+            //    Console.WriteLine(item + " ");
+            //}
+
         }
     }
 }
---
+++ 03. Match Dates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string regex = @"\b[0-9]{2}(.)[A-Z][a-z]{2}\1[0-9]{4}\b";
+            //string regex = @"\b(?<day>\d{2})([.-\/])(?<month>[A-Z][a-z]{2})\2(?<year>[0-9]{4})\b";
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, @"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b");
+            //MatchCollection matches = Regex.Matches(input, regex);
+
+            foreach (Match item in matches)
+            {
+                var day = item.Groups["day"].Value;
+                var month = item.Groups["month"].Value;
+                var year = item.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+
+                //Console.WriteLine(item);
+            }
+
+        }
+    }
+}
