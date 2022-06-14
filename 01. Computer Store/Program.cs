56cd13722035b2fc2b23131d4af9f735809f39c2
FelixVictrix <tinchevp@gmail.com>
2022-06-14 18:09:59 +0300

    0

---
+++ 01. Computer Store/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _01._Computer_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal partsPriceSum = 0;
+            decimal taxesSum = 0;
+            decimal totalPrice = 0;
+
+            string input = Console.ReadLine();
+
+            while (input != "special" && input != "regular")
+            {
+                decimal currPrice = decimal.Parse(input);
+
+                if (currPrice < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    input = Console.ReadLine();
+                    continue;
+                }
+                partsPriceSum += currPrice;
+                taxesSum += currPrice * (decimal)0.20;
+                totalPrice += currPrice * (decimal)1.20;
+
+                input = Console.ReadLine();
+            }
+            if (totalPrice == 0)
+            {
+                Console.WriteLine("Invalid order!");
+                return;
+            }
+            if (input == "special")
+            {
+                totalPrice *= (decimal)0.9;
+            }
+            Console.WriteLine($"Congratulations you've just bought a new computer!\n" +
+                              $"Price without taxes: {partsPriceSum:f2}$\n" +
+                              $"Taxes: {taxesSum:f2}$\n" +
+                              $"-----------\n" +
+                              $"Total price: {totalPrice:f2}$");
+        }
+    }
+}
--- TEST/Program.cs
+++ TEST/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -10,14 +11,28 @@ namespace TEST
         static void Main(string[] args)
         {
 
-            string input = Console.ReadLine();
+            //var people = new[]
+            //{
+            //    ( Name : "Patty", Age : 25, isMale : false ),
+            //    ( Name : "Kenny", Age : 43, isMale : true ),
+            //    ( Name : "Kate", Age : 37, isMale : false )
+            //};
 
-            MatchCollection matches = Regex.Matches(input, @"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+            //var res = people.Where(p => p.Name.StartsWith("K")).Select(p => p);
+            //res = people.Where(p => p.Age >= 40).Select(p => p);
 
-            foreach (Match match in matches)
-            {
-                Console.Write(match + " ");
-            }
+            //foreach (var item in res)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+            string[] words = { "one", "two", "three" };
+
+            var result = words.Select((w, i) => new { Index = i, Value = w }).Where(w => w.Value.Length == 3).ToList();
+
+            Debug.WriteLine("Prints index for words that have a string length of 3:");
+            foreach (var word in result)
+                Debug.WriteLine(word.Index.ToString());
 
         }
     }
