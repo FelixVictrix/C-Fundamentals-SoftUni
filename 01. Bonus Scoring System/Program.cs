c499a9b9f149416c621397565b32e011a08e1539
FelixVictrix <tinchevp@gmail.com>
2022-06-19 17:42:16 +0300

    0

---
+++ 01. Bonus Scoring System/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Bonus_Scoring_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numOfStudents = int.Parse(Console.ReadLine());
+            int numOfLectures = int.Parse(Console.ReadLine());
+            int bonus = int.Parse(Console.ReadLine());
+
+            double maxBonus = 0;
+            double maxLecture = 0;
+
+            for (int i = 0; i < numOfStudents; i++)
+            {
+
+                int attendance = int.Parse(Console.ReadLine());
+                double totalBonus = (attendance * 1.0 / numOfLectures )* (5 + bonus);
+
+                if (totalBonus > maxBonus)
+                {
+                    maxBonus = totalBonus;
+                    maxLecture = attendance;
+                }
+            }
+
+            Console.WriteLine($"Max Bonus: {Math.Ceiling(maxBonus)}.\nThe student has attended {maxLecture} lectures.");
+        }
+    }
+}
