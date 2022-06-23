d446b57c0f3c00503a098d6a513863365bcdebbb
FelixVictrix <tinchevp@gmail.com>
2022-06-23 17:18:01 +0300

    0

---
+++ 03. Moving Target/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03A._Moving_Target
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] action = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = action[0];
+                int index = int.Parse(action[1]);
+
+                if (command == "Shoot")
+                {
+                    if (index >= 0 && index <= list.Count - 1)
+                    {
+                        int power = int.Parse(action[2]);
+                        list[index] -= power;
+                        if (list[index] <= 0)
+                        {
+                            list.RemoveAt(index);
+                        }
+                    }
+                }
+                else if (command == "Add")
+                {
+                    if (index >= 0 && index <= list.Count - 1)
+                    {
+                        int value = int.Parse(action[2]);
+
+                        list.Insert(index, value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid placement!");
+                    }
+
+                }
+                else if (command == "Strike")
+                {
+                    int radius = int.Parse(action[2]);
+                    int startIndex = index - radius;
+                    int endIndex = index + radius;
+
+                    if (startIndex >= 0 && endIndex < list.Count && radius > 0)
+                    {
+
+                        int count = endIndex - startIndex + 1;
+
+                        list.RemoveRange(startIndex, count);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Strike missed!");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join("|", list));
+        }
+    }
+}
--- TEST/Program.cs
+++ TEST/Program.cs
@@ -2,14 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace _03._The_Pianist
+namespace Moving_Target
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            List<int> targets = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
 
-         
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] splitCommand = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string action = splitCommand[0];
+
+                if (action == "Shoot")
+                {
+                    int index = int.Parse(splitCommand[1]);
+                    int power = int.Parse(splitCommand[2]);
+
+                    if (!ValidateIndex(targets, index))
+                    {
+                        Console.WriteLine("Invalid placement!");
+                    }
+                    else
+                    {
+                        targets[index] -= power;
+                        if (targets[index] <= 0)
+                        {
+                            targets.Remove(targets[index]);
+                        }
+                    }
+                }
+
+                if (action == "Add")
+                {
+                    int index = int.Parse(splitCommand[1]);
+                    int value = int.Parse(splitCommand[2]);
+
+                    if (!ValidateIndex(targets, index))
+                    {
+                        Console.WriteLine("Invalid placement!");
+                    }
+                    else
+                    {
+                        targets.Insert(index, value);
+                    }
+                }
+
+                if (action == "Strike")
+                {
+                    int index = int.Parse(splitCommand[1]);
+                    int radius = int.Parse(splitCommand[2]);
+                    int positiveRange = index + radius;
+                    int negativeRange = index - radius;
+
+                    if (index >= 0 && index < targets.Count && positiveRange >= 0 && positiveRange < targets.Count && negativeRange >= 0 && negativeRange < targets.Count)
+                    {
+                        int start = Math.Max(0, index - radius);
+                        int end = Math.Min(targets.Count - 1, index + radius);
+
+                        targets.RemoveRange(start, end);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Strike missed!");
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join("|", targets));
         }
+        static bool ValidateIndex(List<int> targets, int value) => value >= 0 && value < targets.Count;
     }
 }
