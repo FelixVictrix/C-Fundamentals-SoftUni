26e1c20b6b46c1a19c55f64b63f7b12bd04b405f
FelixVictrix <tinchevp@gmail.com>
2022-06-24 16:45:06 +0300

    0

--- 02. Fancy Barcodes/Program.cs
+++ 02. Fancy Barcodes/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace _02._Fancy_Barcodes
 {
@@ -6,8 +8,38 @@ namespace _02._Fancy_Barcodes
     {
         static void Main(string[] args)
         {
-           
+         
+            string barcode = @"@#+([A-Z][A-Za-z\d]{4,}[A-Z])@#+";
 
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                MatchCollection matches = Regex.Matches(input, barcode);
+
+                if (matches.Any())
+                {
+                    foreach (Match item in matches)
+                    {
+                        if(item.Value.Any(c=> char.IsDigit(c)))
+                        {
+                            var digits = item.Value.Where(c=> char.IsDigit(c)).Select(c=> c).ToArray();
+                            Console.WriteLine($"Product group: {string.Join("",digits)}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Product group: 00");
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+
+            }
 
         }
     }
